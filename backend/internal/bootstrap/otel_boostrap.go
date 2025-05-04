d261d9ea487e034a0222f154361a5d4af94e3598
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2025-05-04 11:12:23 -0700

    Fix merge conflicts

--- backend/internal/bootstrap/otel_boostrap.go
+++ backend/internal/bootstrap/otel_boostrap.go
@@ -2,10 +2,11 @@ package bootstrap
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"time"
 
 	"github.com/pocket-id/pocket-id/backend/internal/common"
+	"github.com/pocket-id/pocket-id/backend/internal/utils"
 	"go.opentelemetry.io/contrib/exporters/autoexport"
 	"go.opentelemetry.io/otel"
 	metricnoop "go.opentelemetry.io/otel/metric/noop"
@@ -17,57 +18,32 @@ import (
 	tracenoop "go.opentelemetry.io/otel/trace/noop"
 )
 
-func defaultResource() *resource.Resource {
-	r, err := resource.Merge(
+func defaultResource() (*resource.Resource, error) {
+	return resource.Merge(
 		resource.Default(),
 		resource.NewSchemaless(
 			semconv.ServiceName("pocket-id-backend"),
 			semconv.ServiceVersion(common.Version),
 		),
 	)
-
-	if err != nil {
-		log.Fatalf("Failed to create resource: %s", err)
-	}
-
-	return r
 }
 
-func initMeter(ctx context.Context) metric.Reader {
-	mr, err := autoexport.NewMetricReader(ctx)
+func initOtel(ctx context.Context, metrics, traces bool) (shutdownFns []utils.Service, err error) {
+	resource, err := defaultResource()
 	if err != nil {
-		log.Fatalf("Failed to create OTEL metric reader: %s", err)
-	}
-	return mr
-}
-
-func initTracer(ctx context.Context) sdktrace.SpanExporter {
-	se, err := autoexport.NewSpanExporter(ctx)
-	if err != nil {
-		log.Fatalf("Failed to create OTEL span exporter: %s", err)
-	}
-	return se
-}
-
-func initOtel(ctx context.Context,
-	metrics, traces bool,
-) func() {
-	shutdownFuncs := []func(){}
-	if !traces {
-		otel.SetTracerProvider(tracenoop.NewTracerProvider())
-		shutdownFuncs = append(shutdownFuncs, func() {})
-	}
-	if !metrics {
-		otel.SetMeterProvider(metricnoop.NewMeterProvider())
-		shutdownFuncs = append(shutdownFuncs, func() {})
+		return nil, fmt.Errorf("failed to create OpenTelemetry resource: %w", err)
 	}
 
-	resource := defaultResource()
+	shutdownFns = make([]utils.Service, 0, 2)
 
 	if traces {
+		tr, err := autoexport.NewSpanExporter(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to initialize OpenTelemetry span exporter: %w", err)
+		}
 		tp := sdktrace.NewTracerProvider(
 			sdktrace.WithResource(resource),
-			sdktrace.WithBatcher(initTracer(ctx)),
+			sdktrace.WithBatcher(tr),
 		)
 
 		otel.SetTracerProvider(tp)
@@ -78,34 +54,42 @@ func initOtel(ctx context.Context,
 			),
 		)
 
-		shutdownFuncs = append(shutdownFuncs, func() {
-			tpCtx, tpCancel := context.WithTimeout(context.Background(), 10*time.Second)
+		shutdownFns = append(shutdownFns, func(shutdownCtx context.Context) error {
+			tpCtx, tpCancel := context.WithTimeout(shutdownCtx, 10*time.Second)
 			defer tpCancel()
-			if err := tp.Shutdown(tpCtx); err != nil {
-				log.Printf("Failed to gracefully shut down traces exporter: %s", err)
+			shutdownErr := tp.Shutdown(tpCtx)
+			if shutdownErr != nil {
+				return fmt.Errorf("failed to gracefully shut down traces exporter: %w", shutdownErr)
 			}
+			return nil
 		})
+	} else {
+		otel.SetTracerProvider(tracenoop.NewTracerProvider())
 	}
 
 	if metrics {
+		mr, err := autoexport.NewMetricReader(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to initialize OpenTelemetry metric reader: %w", err)
+		}
 		mp := metric.NewMeterProvider(
 			metric.WithResource(resource),
-			metric.WithReader(initMeter(ctx)),
+			metric.WithReader(mr),
 		)
 
 		otel.SetMeterProvider(mp)
-		shutdownFuncs = append(shutdownFuncs, func() {
-			mpCtx, mpCancel := context.WithTimeout(context.Background(), 10*time.Second)
+		shutdownFns = append(shutdownFns, func(shutdownCtx context.Context) error {
+			mpCtx, mpCancel := context.WithTimeout(shutdownCtx, 10*time.Second)
 			defer mpCancel()
-			if err := mp.Shutdown(mpCtx); err != nil {
-				log.Printf("Failed to gracefully shut down metrics exporter: %s", err)
+			shutdownErr := mp.Shutdown(mpCtx)
+			if shutdownErr != nil {
+				return fmt.Errorf("failed to gracefully shut down metrics exporter: %w", shutdownErr)
 			}
+			return nil
 		})
+	} else {
+		otel.SetMeterProvider(metricnoop.NewMeterProvider())
 	}
 
-	return func() {
-		for _, f := range shutdownFuncs {
-			f()
-		}
-	}
+	return shutdownFns, nil
 }
